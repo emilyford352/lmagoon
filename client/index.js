0241509ca6d0384f00d760e940e2c5f8c2b753ce
Emily Ford <eford@blackducksoftware.com>
2017-09-07 21:16:02 -0400

    Adding react nav

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import MagoonNav from './components/Nav.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MagoonNav />, document.getElementById('nav'));
